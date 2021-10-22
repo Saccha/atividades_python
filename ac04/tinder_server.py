172c9aebb1c871a1d5a6deb11b8fccd96ffb18cb
Sabrina Haßlocher <62216740+Saccha@users.noreply.github.com>
2021-10-21 21:52:29 -0300

    Update tinder_server.py

--- ac04/tinder_server.py
+++ ac04/tinder_server.py
@@ -10,14 +10,5 @@ def listar_pessoas():
     lista = i.todas_as_pessoas()
     return jsonify(lista)
 
-@app.route("/pessoas", methods=["POST"])
-def add_pessoa():
-    dicionario_pessoa = request.json
-    i.append(dicionario_pessoa)
-    return jsonify({"status":"ok"})
 
-@app.route("/pessoas/<int:id_pessoa>")
-def pessoa_por_id(id_pessoa):
-    pessoa = i.localiza_pessoa(id_pessoa)
-    return jsonify(pessoa)
 
